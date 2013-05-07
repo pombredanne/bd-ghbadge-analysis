b0579bffac777eae606d9ae717b71522179fbcf9
Ville Tuulos <ville+bitdeli@bitdeli.com>
2013-05-07 05:00:32 +0000

    add number of repos added

--- __main__.py
+++ __main__.py
@@ -9,77 +9,17 @@ set_theme('eighties')
 TIMELINE_DAYS = 30 
 TFORMAT = '%Y-%m-%d'
                
-geoip = GeoIP.open('/usr/share/geoip/GeoLiteCity.dat', GeoIP.GEOIP_STANDARD)
-
 text = {'window': TIMELINE_DAYS}
 
-def countries(profiles):
-    stats = Counter()
+def growth(profiles):
+    repos = {}
     for profile in profiles:
-        country = geoip.record_by_addr(profile.uid)
-        if country:
-            stats[country['country_code']] += 1
-    yield stats
-
-def activity(profiles):
-    limit = datetime.now() - timedelta(days=TIMELINE_DAYS)
-    limit_str = limit.strftime(TFORMAT)
-        
-    def recent_days(visits):
-        for visit in visits:
-            day = visit['tstamp'].split('T')[0]
-            if day >= limit_str:
-                yield day
-    
-    def timeline(stats):
-        for i in range(TIMELINE_DAYS + 1):
-            day = (limit + timedelta(days=i)).strftime(TFORMAT)
-            yield day, stats[day]
-
-    def popularity(repos):
-        for repo, visits in repos.iteritems():
-            yield sum(visits.itervalues()), repo
-                
-    repos = defaultdict(Counter)
-    for profile in profiles:
-        for repo, visits in profile['repos'].iteritems():
-            repos[repo].update(frozenset(recent_days(visits)))
-
-    if repos:
-        pop = list(popularity(repos))
-        text['popular'] = max(pop)
-        text['total'] = sum(visits for visits, repo in pop)
-        
-        Title("Repos have {total} daily unique visitors in total "
-              "over the last {window} days",
-              text)
-        Description("The most popular repository is *{popular[1]}* with "
-                    "{popular[0]} visitors.",
-                    text)
-        
-        yield {'type': 'text',
-               'size': (12, 1),
-               'head': 'Daily Unique Visitors'}
-        
-        for repo, stats in sorted(repos.iteritems()):
-            yield {'type': 'line',
-                   'label': repo.split('/')[1],
-                   'data': list(timeline(stats)),
-                   'size': (6, 2)}
-    else:
-        Title("First badge added!")
-        Description("It takes about an hour for the real data "
-                    "to start flowing in.")
-        yield {'type': 'text',
-               'size': (12, 1),
-               'color': 3,
-               'head': 'Bummer! No data yet'}
-        yield {'type': 'text',
-               'size': (12, 2),
-               'text': """It may take an hour for the first
-                          visits to appear after you have added the badge
-                          for the first time."""}
-                
+        for repo, events in profile['repos'].iteritems():
+            first = min(e['tstamp'] for e in events)
+            repos[repo.split('?')[0]] = first.split('T')[0]
+    c = Counter(repos.itervalues())
+    yield sorted(c.items())[1:]
+               
 def num_events(profiles):
     s = 0
     for profile in profiles:
@@ -87,6 +27,8 @@ def num_events(profiles):
     yield {'head': s}
     
 Profiles().map(num_events).show('text')    
+Profiles().map(growth).show('line',
+                            size=(12, 4))
 
 #Profiles().map(countries).show('map',
 #                               label='Visitors',
